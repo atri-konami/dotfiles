58926eb950bf664abeb213f2cef48cb0d27a6872
atri-konami <freude1994@gmail.com>
2014-11-21 10:23:21 +0900

    modified init.rb (not tested)

--- init.rb
+++ init.rb
@@ -1,19 +1,42 @@
+# it runs only in Linux.
+
 files=`ls -A .`.split(" ").delete_if{|str| str =~ /git/}
 
 flag = false
+
+tmp_path = File.expand_path("~/dottmp" + Time.now.strftime("_%m%d_%H%M%S"))
+system("mkdir #{tmp_path}")
+
+exec_files=[]
+
 files.each{|fname|
     path = File.expand_path("~") + "/" + fname
+
     if File.exists?(path) then
-        puts "#{path} exists. Please copy the file in other dir and remove that in home dir."
-        flag = true
+        print "#{path} exists. Do you move it to #{tmp_path} ? [y/n]:"
+
+        if gets.chomp =~ /[Yy]/ 
+            system("cp #{path} #{tmp_path}")
+            puts "moved #{path} to #{tmp_path}."
+            exec_files << fname
+            flag = true
+        else
+            system("rm -rf #{tmp_path}")
+            exit(false)
+        end
     end
 }
 
-if flag then exit(false) end
+if !flag then
+    system("rm -rf #{tmp_path}")
+end
 
-files.each{|fname|
-    path = File.expand_path("~") + "/" + fname
-    system("ln -s #{path} #{fname}")
+exec_files.each{|fname|
+    gen_path = File.expand_path("~") + "/" + fname
+    der_path = Dir.pwd + "/" + fname
+
+    system("rm #{gen_path}")
+    system("ln -s #{der_path} #{gen_path}")
     puts "put a synbolic link for #{path} , derived from dotfiles/#{fname}."
 }
 
